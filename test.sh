1f61796c2dd7f8646af57157b947b85b610976a7
rosstimo <rosstimo@isu.edu>
2023-11-08 14:12:42 -0700

    functional test.sh

--- test.sh
+++ test.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
+#make sure the list of tor exit nodes is up to date
+TOR_EXIT_LIST="tor_exit_list.txt"
+# check if $TOR_EXIT_LIST exists. If not run script update_tor_exit_list.sh and abort if error
+if [ ! -f $TOR_EXIT_LIST ]; then
+  echo "$TOR_EXIT_LIST not found. Running update_tor_exit_list.sh"
+  ./update_tor_exit_list.sh || { echo >&2 "Error running update_tor_exit_list.sh. Aborting."; exit 1; }
+fi
+
+# check if $TOR_EXIT_LIST is older than 1 day. If yes run script update_tor_exit_list.sh
+if test `find "$TOR_EXIT_LIST" -mtime +1`; then
+  echo "$TOR_EXIT_LIST is older than 1 day. Running update_tor_exit_list.sh"
+  ./update_tor_exit_list.sh || { echo >&2 "Error running update_tor_exit_list.sh. Aborting."; exit 1; }
+fi
+
 # run ss -tupn and store the output in a variable. if ss is not installed otherwise exit
 ss_output=$(ss -tupn) || { echo >&2 "ss not installed.  Aborting."; exit 1; }
 
-TOR_EXIT_LIST="tor_exit_list.txt"
-# check if tor_exit_list.txt exists. If not run script update_tor_exit_list.sh and abort if error
-if [ ! -f tor_exit_list.txt ]; then
-  echo "tor_exit_list.txt not found. Running update_tor_exit_list.sh"
-  ./update_tor_exit_list.sh || { echo >&2 "Error running update_tor_exit_list.sh. Aborting."; exit 1; }
-fi
-
-# check if tor_exit_list.txt is older than 1 day. If yes run script update_tor_exit_list.sh
-if test `find "tor_exit_list.txt" -mtime +1`; then
-  echo "tor_exit_list.txt is older than 1 day. Running update_tor_exit_list.sh"
-  ./update_tor_exit_list.sh || { echo >&2 "Error running update_tor_exit_list.sh. Aborting."; exit 1; }
-fi
-
-# extract the ip address and process name from each line of $ss_output and stor them in variables
+# extract the ip address and process name from each line of $ss_output
 while read -r line; do
-  # if not blank line and line begins with tcp or udp else skip to next line
-  # if [[ $line != "" ]] && [[ $line != tcp* ]] && [[ $line != udp* ]]; then
+  REMOTE_IP=""
+  REMOTE_PORT=""
+  # line begins with tcp or udp else skip to next line
   if [[ $line == tcp* ]] || [[ $line == udp* ]]; then
 
-    LOG_RECORD="$line"
     # Extract the remote IP address and port
-    REMOTE_IP_PORT=$(echo "$LOG_RECORD" | awk '{print $6}')
-    REMOTE_IP=""
-    REMOTE_PORT=""
+    REMOTE_IP_PORT=$(echo "$line" | awk '{print $6}')
 
     # if REMOTE_IP_PORT contains "[", then it is an IPv6 address]"
     # extract the IP and PORT accordingly
@@ -39,26 +38,22 @@ while read -r line; do
     fi
 
     # extract the process name assuming it is in the last column
-    PROCESS=$(echo "$LOG_RECORD" | awk '{print $NF}')
+    PROCESS=$(echo "$line" | awk '{print $NF}')
 
     # set HEADER to current date and time
     HEADER=$(date +%Y-%m-%d_%H:%M:%S)
-    echo "$HEADER, $REMOTE_IP, $PROCESS" 
 
     # Check if the IP address is in the TOR exit tor_exit_list
     if grep -Fxq "$REMOTE_IP" "$TOR_EXIT_LIST"; then
-      #echo "IP address $REMOTE_IP is in the TOR exit tor_exit_list"
 
-     # Add the IP address to the firewall
-     # iptables -A INPUT -s $REMOTE_IP -j DROP
+       # Add the IP address to the firewall
+       # iptables -A INPUT -s $REMOTE_IP -j DROP
 
-     # append the IP address and process name to a log file with a timestamp formatted as YYYY-MM-DD-HH
-     # echo "$HEADER, $REMOTE_IP, $PROCESS" >> /var/log/tor_exit_node_hits.log
-     echo "$HEADER, $REMOTE_IP, $PROCESS" >> tor_exit_node_hits-$(date +%Y-%m-%d-%H).log
-   else
-     #echo "IP address $REMOTE_IP is not in the TOR exit tor_exit_list"
-     continue
+       # append the IP address and process name to a log file with a timestamp formatted as YYYY-MM-DD-HH
+       # echo "$HEADER, $REMOTE_IP, $PROCESS" >> /var/log/tor_exit_node_hits.log
+       echo "$HEADER, $REMOTE_IP, $PROCESS" >> tor_exit_node_hits-$(date +%Y-%m-%d-%H).log
     fi
+    echo "$HEADER $line" >> ss_output-$(date +%Y-%m-%d-%H).log 
+    echo "$HEADER, $REMOTE_IP, $PROCESS" 
   fi 
-
 done <<< "$ss_output"
